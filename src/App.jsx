9cf5bba89426bb73b0e5e176f95af9d5cfcfe18f
Koushik Mallik <koushikmallik001@gmail.com>
2024-03-15 20:55:21 +0530

    added login modal added prettier config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Header from "./components/common/header/index.jsx";
-import Footer from "./components/common/footer/index.jsx";
-import Homepage from "./pages/home/index.jsx";
+import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Header from './components/common/header/index.jsx'
+import Footer from './components/common/footer/index.jsx'
+import Homepage from './pages/home/index.jsx'
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          {/*                     <Route path="/dashboard" element={<Dashboard/>}/> */}
-          {/*<Route path="/edit-profile" element={<EditProfile/>} />*/}
-          {/*                     <Route path="/groups" element={<Dashboard/>}/> */}
-          {/*                     <Route path="/friends" element={<Dashboard/>}/> */}
-          {/*                     <Route path="/login" element={<Loginpage/>}/> */}
-          {/*                     <Route path="/register" element={<RegisterPage/>}/> */}
-          {/*<Route path="*" element={<PageNotFound/>} />*/}
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    {/*                     <Route path="/dashboard" element={<Dashboard/>}/> */}
+                    {/*<Route path="/edit-profile" element={<EditProfile/>} />*/}
+                    {/*                     <Route path="/groups" element={<Dashboard/>}/> */}
+                    {/*                     <Route path="/friends" element={<Dashboard/>}/> */}
+                    {/*                     <Route path="/login" element={<Loginpage/>}/> */}
+                    {/*                     <Route path="/register" element={<RegisterPage/>}/> */}
+                    {/*<Route path="*" element={<PageNotFound/>} />*/}
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
