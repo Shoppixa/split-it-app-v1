894214d7bd76a59853024424515ad25fa5fd7cca
Koushik Mallik <koushikmallik001@gmail.com>
2024-03-17 21:52:03 +0530

    added login Page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
-
+import logo from '../../assets/brand/brand-big.png'
+import LoginForm from '../../components/loginpage/index.jsx'
 const LoginPage = () => {
-    return <div>LoginPage</div>
+    return (
+        <div className="container">
+            <div className="text-center my-2">
+                <img
+                    src={logo}
+                    alt="my_logo"
+                    className="img-fluid mt-5 mb-5"
+                    style={{ maxHeight: '200px' }}
+                />
+            </div>
+            <LoginForm />
+        </div>
+    )
 }
 export default LoginPage
